90fda309145244d465d66fbb7209619d83010f6c
Michael Fladischer <michael@fladi.at>
2020-08-04 21:15:05 +0200

    Django 2.1 compatibility.

--- src/outpost/django/typo3/urls.py
+++ src/outpost/django/typo3/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "typo3"
+
 urlpatterns = [
     url(r"^media/(?P<pk>\d+)/(?P<width>\d+)?$", views.MediaView.as_view(), name="media")
 ]
