a1f7d1b1a3ab48a606e033f348eb5c520a95ac07
Michael Fladischer <michael@fladi.at>
2021-03-10 16:15:30 +0100

    Use absolute import for settings.

--- src/outpost/django/typo3/migrations/0017_media.py
+++ src/outpost/django/typo3/migrations/0017_media.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-from ..conf import settings
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
